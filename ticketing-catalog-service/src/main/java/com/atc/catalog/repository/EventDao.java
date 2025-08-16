6b35e068e5fe449a4d242e73531805f083616536
Ali Talha ÇOBAN <cobanalitalha@gmail.com>
2025-08-16 16:21:00 +0300

    Refine event query method

--- ticketing-catalog-service/src/main/java/com/atc/catalog/repository/EventDao.java
+++ ticketing-catalog-service/src/main/java/com/atc/catalog/repository/EventDao.java
@@ -1,9 +1,18 @@
 package com.atc.catalog.repository;
 
 import com.atc.catalog.entity.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.OffsetDateTime;
+
 @Repository
 public interface EventDao extends JpaRepository<Event, Long> {
+
+    Page<Event> findByStartAtBetweenAndHallVenueCityIgnoreCase(OffsetDateTime from,
+                                                               OffsetDateTime to,
+                                                               String city,
+                                                               Pageable pageable);
 }
